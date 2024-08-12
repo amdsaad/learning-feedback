2d7c7d01740cb4b41279e387b5fd525a0abb9ec6
amdsaad <saadamd@gmail.com>
2024-08-12 23:52:44 +0300

    middleware

---
+++ middleware/auth.ts
@@ -0,0 +1,7 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+    const user = useStrapiUser();
+
+    if (!user.value) {
+        return navigateTo("/login");
+    }
+});
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -5,6 +5,9 @@
 </template>
 
 <script lang="ts" setup>
+definePageMeta({
+  middleware: ["auth"],
+})
 
 </script>
 
