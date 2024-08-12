78125d0f38d1d313c8421d0e67a2eb9f8df82e07
amdsaad <saadamd@gmail.com>
2024-08-12 21:15:55 +0300

    the header

---
+++ components/Shared/TheHeader.vue
@@ -0,0 +1,43 @@
+<template>
+  <header class="py-4">
+    <nav class="container mx-auto flex justify-between items-center">
+      <NuxtLink to="/">
+        LOGO
+      </NuxtLink>
+      <ul
+        class="flex items-center gap-4"
+      >
+        <li
+          v-for="link in links"
+          :key="link.id"
+        >
+          <NuxtLink :to="link.href">
+            {{ link.name }}
+          </NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts" setup>
+const links = ref([
+  {
+    id: 1,
+    name: 'Dashboard',
+    href: '/dashboard',
+  },
+  {
+    id: 2,
+    name: 'Projects',
+    href: '/projects',
+  },
+  {
+    id: 3,
+    name: 'Settings',
+    href: '/settings',
+  },
+])
+</script>
+
+<style></style>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <SharedTheHeader/>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
